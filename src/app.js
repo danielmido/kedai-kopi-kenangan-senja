510f16c451a067f3fc96b9d7ecf000e7928e84af
Daniel Mido <danielmido30@outlook.com>
2025-04-26 19:45:41 +0700

    video 5

--- src/app.js
+++ src/app.js
@@ -68,6 +68,63 @@ document.addEventListener('alpine:init', () => {
   });
 });
 
+// form validation
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function() {
+  for(let i = 0; i < form.elements.length; i++) {
+    if(form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove('disabled');
+      checkoutButton.classList.add('disabled');
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove('disabled');
+})
+
+// kirim data ketika tombol checkout di-klik
+checkoutButton.addEventListener('click', async function(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  /*
+  ==== deactivated feature down here: ====
+  const message = formatMessage(objData);
+  window.open('http://wa.me/PHONE_NUMBER?text=' + encodeURIComponent(message));
+  */
+
+  // minta transaction token menggunakan ajax / fetch
+  try {
+    const response = await fetch('php/placeOrder.php', {
+      method: 'POST',
+      body: data,
+    });
+    const token = await response.text();
+    window.snap.pay(token);
+  } catch(err) {
+    console.log(err.message);
+  }
+});
+
+// format pesan whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No HP: ${obj.phone}
+    Data Pesanan: ${JSON.parse(obj.items).map((item) => `\n${item.name} (${item.quantity} x ${rupiah(item.total)})`)}
+    Total: ${rupiah(obj.total)}
+    Terima kasih.
+  `;
+}
+
 // konversi ke Rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat('id-ID', {
